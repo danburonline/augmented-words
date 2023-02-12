d8ff5e14fee39aa578191fa457ba4fa5d693c411
Daniel Burger <public@danielburger.online>
2023-02-12 19:09:48 +0100

    Possibly update sphere's position on every frame

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,17 +1,25 @@
 import { XR, Hands, useXR } from '@react-three/xr';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import CustomARButton from './CustomARButton';
 import { Environment, Grid, Stage } from '@react-three/drei';
+import { useRef } from 'react';
 
 function FingerTipSphere() {
   const xr = useXR();
+  const meshRef = useRef<THREE.Mesh | null>(null);
 
-  console.log(xr.controllers[1]?.hand.joints['index-finger-tip']?.position);
+  useFrame(() => {
+    if (meshRef.current && xr.controllers[1]?.hand.joints['index-finger-tip']) {
+      meshRef.current.position.set(
+        xr.controllers[1].hand.joints['index-finger-tip'].position.x,
+        xr.controllers[1].hand.joints['index-finger-tip'].position.y,
+        xr.controllers[1].hand.joints['index-finger-tip'].position.z
+      );
+    }
+  });
 
   return (
-    <mesh
-      position={xr.controllers[1]?.hand.joints['index-finger-tip']?.position}
-    >
+    <mesh ref={meshRef}>
       <sphereGeometry args={[0.01, 0.01, 0.01]} />
       <meshStandardMaterial color='red' />
     </mesh>
